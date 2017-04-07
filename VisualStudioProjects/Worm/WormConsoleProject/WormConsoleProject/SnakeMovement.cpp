890d19419ff10e18a4eae5c33c27ce23c3563df0
Joel Åman <Schoelxz@gmail.com>
2017-04-07 16:29:58 +0200

    Base work mostly complete. Thereis: Visuals. Unlimited worm possible (hold Space to grow). Worm parts follow eachother up to head. And more...

---
+++ VisualStudioProjects/Worm/WormConsoleProject/WormConsoleProject/SnakeMovement.cpp
@@ -0,0 +1,134 @@
+#include "SnakeMovement.h"
+
+void SnakeMovement::moveSnake(int& posX, int& posY)
+{
+		if (keyboardState[sf::Keyboard::D] == true)
+			moveKeys = D;
+		if (keyboardState[sf::Keyboard::W] == true)
+			moveKeys = W;;
+		if (keyboardState[sf::Keyboard::S] == true)
+			moveKeys = S;
+		if (keyboardState[sf::Keyboard::A] == true)
+			moveKeys = A;
+		
+	switch (moveKeys)
+	{
+	case W:
+		snakeDir = UP;
+		cout << "W" << endl;
+		moveKeys = NOKEY;
+		break;
+	case A:
+		snakeDir = LEFT;
+		cout << "A" << endl;
+		moveKeys = NOKEY;
+		break;
+	case S:
+		snakeDir = DOWN;
+		cout << "S" << endl;
+		moveKeys = NOKEY;
+		break;
+	case D:
+		snakeDir = RIGHT;
+		cout << "D" << endl;
+		moveKeys = NOKEY;
+		break;
+	case NOKEY:
+		break;
+	default:
+		break;
+	}
+
+	switch (snakeDir)
+	{
+	case UP:
+		posY -= 1;
+		cout << "UP";
+		break;
+	case LEFT:
+		posX -= 1;
+		cout << "LEFT";
+		break;
+	case DOWN:
+		posY += 1;
+		cout << "DOWN";
+		break;
+	case RIGHT:
+		posX += 1;
+		cout << "RIGHT";
+		break;
+	case START:
+		break;
+	default:
+		break;
+	}
+	
+	if (posX >= 10)
+		posX = 0;
+	if (posX < 0)
+		posX = 9;
+	if (posY >= 10)
+		posY = 0;
+	if (posY < 0)
+		posY = 9;
+}
+
+SnakeMovement::SnakeMovement()
+{
+	for (int i = 0; i < 256; i++)
+		keyboardState[i] = false;
+
+	snakeDir = START;
+	moveKeys = NOKEY;
+}
+	/*
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !flag1)
+	{
+		posY -= 1;
+		cout << "W" << endl;
+		flag1 = true;
+	}
+	else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		flag1 = false;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !flag2)
+	{
+		posX -= 1;
+		cout << "A" << endl;
+		flag2 = true;
+	}
+	else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		flag2 = false;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && !flag3)
+	{
+		posY += 1;
+		cout << "S" << endl;
+		flag3 = true;
+	}
+	else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		flag3 = false;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !flag4)
+	{
+
+		cout << "D" << endl;
+		flag4 = true;
+	}
+	else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		flag4 = false;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && !flag5)
+	{
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		//updatePlayfield(playField, posY, posX);
+		cout << "SPACE" << endl;
+		flag5 = true;
+	}
+	else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+		flag5 = false;
+}*/
+
+
+SnakeMovement::~SnakeMovement()
+{
+}
--- VisualStudioProjects/Worm/WormConsoleProject/WormConsoleProject/main.cpp
+++ VisualStudioProjects/Worm/WormConsoleProject/WormConsoleProject/main.cpp
@@ -1,145 +1,140 @@
 #include <SFML/Graphics.hpp>
 #include "TwoDSpace.h"
+#include "SnakeMovement.h"
+#include "SnakeBody.h"
 
 using namespace sf;
 using namespace std;
 
-void draw(int asdf[][])
-{
-	for (int i = 0; i < 30; i++)
-	{
-		std::cout << std::endl;
-		for (int j = 0; j < 30; j++)
-		{
-			std::cout << asdf[i][j];
 
-		}
-	}
-}
-void updatePlayfield(int playField[][], int posX, int posY)
-{
-	for (int i = 0; i < playSize; i++)
-	{
-		std::cout << std::endl;
-		for (int j = 0; j < playSize; j++)
-		{
-			if (!(posX == i && posY == j))
-				playField[i][j] = 0;
-			else
-				playField[i][j] = 1;
-			std::cout << playField[i][j];
 
-		}
-	}
-	
-}
+/**/
+
+
 
 int main()
 {
+
+	TwoDSpace twoDSpace;
+	SnakeMovement snakeMovement;
+
+	Vector2<float> headPos;
+
+
+
+	const int numFramesPerSecond = 2;
 	sf::Clock clock;
+	const float updateInterval = 1.0f / (numFramesPerSecond);
 
+	vector<SnakeBody*> snakeBody;
+	
 
-	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
 	sf::RectangleShape shape;
-	shape.setSize(sf::Vector2f(100, 100));
-	shape.setFillColor(sf::Color::Green);
+	sf::RectangleShape shape2;
+	sf::RectangleShape shape3;
+	sf::RectangleShape shape4;
+	sf::RectangleShape shape5;
+	shape.setSize(sf::Vector2f(50, 50));
+	shape.setFillColor(sf::Color::Blue);
+	shape2.setSize(sf::Vector2f(50, 50));
+	shape2.setFillColor(sf::Color::Green);
+	shape3.setSize(sf::Vector2f(50, 50));
+	shape3.setFillColor(sf::Color::Green);
+	shape4.setSize(sf::Vector2f(50, 50));
+	shape4.setFillColor(sf::Color::Green);
+	shape5.setSize(sf::Vector2f(50, 50));
+	shape5.setFillColor(sf::Color::Green);
 	
-	
+
+	/*Snake needs to be 2blocks atleast to start (2 push_backs!)*/
+	snakeBody.push_back(new SnakeBody());
+	snakeBody.push_back(new SnakeBody());
+	snakeBody[0]->rect.setFillColor(sf::Color::Red);
 
 	sf::Vector2f velocity(100, 0);
-
-	int playerPositionX;
-	int playerPositionY;
-
-
-	int flag1 = false;
-	int flag2 = false;
-	int flag3 = false;
-	int flag4 = false;
-	int flag5 = false;
-
-	int posX = 0;
-	int posY = 0;
-
-
-
 	
-	
-	
-
+	float currentTime = 0.0f;
 
 	while (window.isOpen())
 	{
 		sf::Time dt = clock.restart();
+		currentTime += dt.asSeconds();
 		sf::Event event;
-		while (window.pollEvent(event))
+		
+		if (currentTime >= updateInterval) //Update Timer
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+				else if (event.type == sf::Event::KeyPressed)
+				{
+					snakeMovement.keyboardState[event.key.code] = true;
+				}
+			}
+
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+			{
+				//twoDSpace.updatePlayfield(twoDSpace.playField, twoDSpace.posX, twoDSpace.posY);
+				//cout << twoDSpace.posX << " and " << twoDSpace.posY << endl;
+
+				snakeBody.push_back(new SnakeBody());
+			}
+
+			snakeMovement.moveSnake(twoDSpace.posX, twoDSpace.posY); //Moves the Digital Snake
+			for (int i = 0; i < 256; i++)
+				snakeMovement.keyboardState[i] = false;
+
+			shape5.setPosition(shape4.getPosition());
+			shape4.setPosition(shape3.getPosition());
+			shape3.setPosition(shape2.getPosition());
+			shape2.setPosition(shape.getPosition());
+			
+			/*snakeBody[7]->rect.setPosition(snakeBody[6]->rect.getPosition());
+			snakeBody[6]->rect.setPosition(snakeBody[5]->rect.getPosition());
+			snakeBody[5]->rect.setPosition(snakeBody[4]->rect.getPosition());
+			snakeBody[4]->rect.setPosition(snakeBody[3]->rect.getPosition());
+			snakeBody[3]->rect.setPosition(snakeBody[2]->rect.getPosition());
+			snakeBody[2]->rect.setPosition(snakeBody[1]->rect.getPosition());
+			snakeBody[1]->rect.setPosition(snakeBody[0]->rect.getPosition());*/
+			
+			if (snakeBody.size() >= 2)
+			{
+				for (int i = snakeBody.size()-1; i >= 1; i--)
+				{
+					snakeBody[i]->rect.setPosition(snakeBody[i - 1]->rect.getPosition());
+				}
+			}
+			snakeBody[0]->rect.setPosition((twoDSpace.posX * 50), (twoDSpace.posY * 50)); //Snake Head
+
+			twoDSpace.updatePlayfield(twoDSpace.playField, twoDSpace.posX, twoDSpace.posY);
+			currentTime -= updateInterval;
 		}
 
-
-		if (posX > 29)
-			posX = 29;
-		if (posX < 0)
-			posX = 0;
-		if (posY > 29)
-			posY = 29;
-		if (posY < 0)
-			posY = 0;
 		
 		
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !flag1)
-		{
-			posY -= 1;
-			cout << "W" << endl;
-			flag1 = true;
-		}
-		else if(!sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-			flag1 = false;
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !flag2)
-		{
-			posX -= 1;
-			cout << "A" << endl;
-			flag2 = true;
-		}
-		else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-			flag2 = false;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && !flag3)
-		{
-			posY += 1;
-			cout << "S" << endl;
-			flag3 = true;
-		}
-		else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-			flag3 = false;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !flag4)
-		{
-			posX += 1;
-			cout << "D" << endl;
-			flag4 = true;
-		}
-		else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-			flag4 = false;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && !flag5)
-		{
-			std::cout << std::endl;
-			std::cout << std::endl;
-			std::cout << std::endl;
-			updatePlayfield(playField, posY, posX);
-			cout << "SPACE" << endl;
-			flag5 = true;
-		}
-		else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-			flag5 = false;
 		
 		window.clear();
+		if (snakeBody.size() >= 2)
+		{
+			for (int i = snakeBody.size()-1; i >= 0; i--)
+			{
+				window.draw(snakeBody[i]->rect);
+			}
+		}
+		
 		window.draw(shape);
+		//window.draw(shape2);
+		//window.draw(shape3);
+		//window.draw(shape4);
+		//window.draw(shape5);
 		window.display();
 		
 
 	}
+	
 
 	return 0;
 }
